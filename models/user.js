00f44d290c87337b6be5a707c14624aaa2efcb33
Yatindra Deo <101984753+hacker-yati12@users.noreply.github.com>
2025-03-30 03:08:19 +0530

    made some changes

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+//Define the Person schema
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        require: true
+    },
+    email: {
+        type: String
+    },
+    mobile: {
+        type: String
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    aadharCardNumber: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
